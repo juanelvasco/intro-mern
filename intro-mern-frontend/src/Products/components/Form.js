440ca1efb1116bb02064a1125c5568f941bde739
Juan Hiribarren <juanelvasco@gmail.com>
2021-10-04 15:36:33 -0300

    subido hasta el vid 20. No funciona el boton add

---
+++ intro-mern-frontend/src/Products/components/Form.js
@@ -0,0 +1,83 @@
+import React, { useState, useRef } from "react"
+import { Form as BulmaForm, Button } from 'react-bulma-components'
+
+const { Field, Control, Label, Input } = BulmaForm
+
+const Form = () => { 
+    const [formValues, setFormValues] = useState({
+        name: '',
+        priceUnitary: '',
+        size: '',
+        description: '',
+    })
+
+    const inputFileRef = useRef()
+
+    const handleCHange = (event) => {
+        const { name, value } = event.target
+        console.log(name, value)
+
+        setFormValues({ ...formValues, [name]: value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formValues)
+        console.log(inputFileRef.current.files)
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+        <Field>
+            <Label>Name</Label>
+            <Control>
+                <Input placeholder= "Text input" 
+                name="name"
+                value={formValues.name}
+                onChange={handleCHange}
+                />
+            </Control>
+        </Field>
+        <Field>
+            <Label>Size</Label>
+            <Control>
+                <Input placeholder= "Text input" 
+                name="size"
+                type="number"
+                value={formValues.size}
+                onChange={handleCHange}
+                />
+            </Control>
+        </Field>
+        <Field>
+            <Label>Unitary price</Label>
+            <Control>
+                <Input placeholder= "Text input" 
+                name="priceUnitary"
+                type="number"
+                value={formValues.priceUnitary}
+                onChange={handleCHange}
+                />
+            </Control>
+        </Field>
+        <Field>
+            <Label>Description</Label>
+            <Control>
+                <Input placeholder= "Text input" 
+                name="description"
+                value={formValues.description}
+                onChange={handleCHange}
+                />
+            </Control>
+        </Field>
+        <Field>
+            <Label>Image</Label>
+            <Control>
+                <Input type= "file" ref={inputFileRef}/>
+            </Control>
+        </Field>
+        <Button type="submit" color="primary">Save</Button>
+      <form/>
+    )    
+}
+
+export default Form
--- intro-mern-frontend/src/Products/components/ProductLayout.js
+++ intro-mern-frontend/src/Products/components/ProductLayout.js
@@ -3,28 +3,30 @@ import { Modal } from 'react-bulma-components'
 import Header from './Header'
 import AddButton from './AddButton'
 import ListProducts from './ListProducts'
-
+import Form from './Form'
 
 
 const ProductLayout = () => {
     const [isModalOpen, setIsModalOpen] = useState(false)
 
     return  (
-    <div>
+    <>
         <Header title="Products app" />
         <AddButton onClick={() => setIsModalOpen(true)}></AddButton>
         <ListProducts/>
         <Modal show={isModalOpen} onClose={() => setIsModalOpen(false)}>
             <Modal.Card>
-                <Modal.Card.Head>
-                    Form
+                <Modal.Card.Head showClose= {false}>
+                    <Modal.Card.Title>
+                        Add Product
+                    </Modal.Card.Title>
                 </Modal.Card.Head>
                 <Modal.Card.Body>
-                    Formulario aqui
+                    <Form />
                 </Modal.Card.Body>
             </Modal.Card>
         </Modal>
-    </div>
+    </>
     )
 }
 
